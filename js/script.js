760df7f029231c5e48659e10902336acbed5e72a
Marine <marinekislova@gmail.com>
2025-02-10 17:10:11 +0200

    2025/02/10

--- js/script.js
+++ js/script.js
@@ -36,14 +36,59 @@ window.addEventListener("DOMContentLoaded", () => {
   if (document.querySelector(".testimonial__author") !== null) {
   }
 
-  if (document.querySelector(".category-content") !== null) {
+  // переход на таб другой страницы
+  const urlParams = new URLSearchParams(window.location.search);
+  const tabId = urlParams.get("tab"); //контейнер с табами
+  const tabLink = document.querySelectorAll(".category-tab__tab"); //ссылки на табы внутри контейнера с табами
+  const tabContent = document.querySelectorAll(".category-content__content"); //контейнеры с контентом табов
+  const contentHeader = document.querySelectorAll(".category-content__header"); //заголовок контента таба
+
+  if (tabId) {
+    // Деактивируем все табы и контент
+    tabLink.forEach((tab) => {
+      tab.classList.remove("active");
+      // tab.style.display = "none";
+    });
+    tabContent.forEach((content) => {
+      // content.classList.remove("active");
+      content.style.display = "none";
+    });
+    contentHeader.forEach((header) => {
+      // header.classList.remove("active");
+      header.style.display = "none";
+    });
+
+    // Активируем нужный таб и соответствующий контент
+    const selectedTab = document.querySelector(
+      `.category-tab__tab[data-tab="${tabId}"]`
+    );
+
+    const selectedContent = document.querySelector(
+      `.category-content__content[data-tab="${tabId}"]`
+    );
+
+    const selectedContentHeader = document.querySelector(
+      `.category-content__header[data-tab="${tabId}"]`
+    );
+
+    console.log(selectedTab, selectedContent, selectedContentHeader);
+
+    if (selectedTab && selectedContent && selectedContentHeader) {
+      selectedTab.classList.add("active");
+      // selectedTab.style.display = "block";
+      selectedTab.style.backgroundColor = "yellow";
+      selectedContent.classList.add("active");
+      selectedContent.style.display = "block";
+      selectedContentHeader.classList.add("active");
+      selectedContentHeader.style.display = "block";
+    }
+  } else if (document.querySelectorAll(".category-content__conent")) {
     tabs({
-      selectorContent: ".category-content",
-      selectorHeader: ".category-content__header",
       selectorTab: ".category-tab__tab",
-      // tabsLink: ".category__link",
+      selectorContent: ".category-content__content",
+      selectorHeader: ".category-content__header",
       selectorParent: ".category-section",
-      activeClass: ".category-tab__tab-active",
+      // activeClass: "active",
     });
   }
 
